06bcb112cc3b5e62ace719fb3900f3b888e29f63
Dylan Leard <dleard@uvic.ca>
2019-01-14 22:16:13 +0000

    added Auth && modified gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,23 @@
 const request = require('request');
+const secrets = require('secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  const options = {
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': secrets.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("lighthouse-labs", "tweeter", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
