91551b1246e85f1127f3aac70c1adce0ff222b08
Dylan Leard <dleard@uvic.ca>
2019-01-15 00:33:55 +0000

    fixed linter errors

--- download_avatars.js
+++ download_avatars.js
@@ -7,7 +7,7 @@ console.log('Welcome to the GitHub Avatar Downloader');
 function getRepoContributors(cb) {
   const repoOwner = process.argv[2];
   const repoName = process.argv[3];
-  if (!repoOwner || !repoName) return console.log('Invalid input. \n correct syntax: node download_avatars.js <repoOwner> <repoName>');
+  if (!repoOwner || !repoName) { return console.log('Invalid input. \n correct syntax: node download_avatars.js <repoOwner> <repoName>'); }
   const options = {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
@@ -17,8 +17,8 @@ function getRepoContributors(cb) {
   };
 
   request(options, function(err, res, body) {
-    body = JSON.parse(body);
-    cb(err, body);  
+    bodyParsed = JSON.parse(body);
+    cb(err, bodyParsed);
   });
 }
 
@@ -27,14 +27,14 @@ function downloadImageByURL(url, filePath) {
     .on('error', function(error) {
       throw error;
     })
-    .pipe(fs.createWriteStream(filePath))
+    .pipe(fs.createWriteStream(filePath));
 }
 
 getRepoContributors(function(err, result) {
-  if (err) console.log("Errors:", err);
+  if (err) { console.log("Errors:", err); }
   for (let i = 0; i < result.length; i++) {
     const filePath = 'avatars/' + result[i].login + '.jpg';
     downloadImageByURL(result[i].avatar_url, filePath);
   }
-  if(result) console.log('Images downloaded');
+  if (result) { console.log('Images downloading'); }
 });
